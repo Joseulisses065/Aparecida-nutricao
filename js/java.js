2ca83676a0bce243132dc6a401013f0734ea6a88
joseulisses065 <sulisses111@gmail.com>
2023-07-07 21:03:18 -0200

    Inclusão de filtros de valores e calculo de imc

--- js/java.js
+++ js/java.js
@@ -1,7 +1,24 @@
-var paciente = document.querySelector("#paciente-01")
-var pesos = paciente.querySelector(".info-peso")
-var altura = paciente.querySelector(".info-altura")
-var imc= paciente.querySelector(".info-imc")
+var pacientes = document.querySelectorAll(".paciente")
 
-imc.innerHTML = (pesos.textContent/(altura.textContent*2));
+
+
+
+pacientes.forEach(paciente => {
+    var peso = paciente.querySelector(".info-peso")
+    var altura = paciente.querySelector(".info-altura")
+
+    if(peso.textContent <=0 || peso.textContent>=1000){
+        alert(`O peso de ${ paciente.querySelector(".info-nome").textContent} é invalido`)
+
+    }else {if(altura.textContent <=0 || altura.textContent>=1000){
+        alert(`A altura de ${ paciente.querySelector(".info-nome").textContent} é invalida`)
+
+    }else{
+        var imc= paciente.querySelector(".info-imc")
+        imc.innerHTML = (peso.textContent/(altura.textContent*altura.textContent)).toFixed(2);
+    }}
+}
+);
+
+
     
